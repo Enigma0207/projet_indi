5d70c0b704de2e879e6bee7371a42524a32e1121
alin <toto@gmail.com>
2023-11-08 23:17:28 +0100

    fin des boutons

--- listeCreneaux.php
+++ listeCreneaux.php
@@ -28,25 +28,27 @@ if (isset($_SESSION["id_user"])) {
                 </tr>
             </thead>
             <tbody>
-                    <?php foreach ($listcreneaux as $creneau) { ?>
-                        <tr class="bg-body-secondary <?= $creneau['disponibilite'] === 'pris' ? 'reserved' : ''; ?>">
-                             <td><?= $creneau['id_creneaux']; ?></td>
-                             <td><?= $creneau['date']; ?></td>
-                             <td><?= $creneau['titre']; ?></td>
-                             <td><?= $creneau['firstname']; ?></td>
-                             <td><?= $creneau['disponibilite']; ?></td>
-                            <td>
-                                <?php if ($creneau['disponibilite'] === 'dispo') { ?>
-                                    <a class="reserannu" href="reserver.php?id=<?= $creneau['id_creneaux']; ?>">Réserver</a>
-                                    <a class="reserannu" href="annuler.php?id=<?= $creneau['id_creneaux']; ?>">Annuler</a>
-                                <?php } elseif ($creneau['disponibilite'] === 'pris' && $creneau['id_eleve'] == $_SESSION["id_user"]) { ?>
-                                    <a href="annuler.php?id=<?= $creneau['id_creneaux']; ?>">Annuler</a>
-                                <?php } else { ?>
-                                    <span>Réservé</span>
-                                <?php } ?>
-                            </td>
-                        </tr>
-                    <?php } ?>
+            <?php foreach ($listcreneaux as $creneau) { ?>
+                <tr class="bg-body-secondary <?= $creneau['disponibilite'] === 'pris' ? 'reserved' : ''; ?>">
+                    <td><?= $creneau['id_creneaux']; ?></td>
+                    <td><?= $creneau['date']; ?></td>
+                    <td><?= $creneau['titre']; ?></td>
+                    <td><?= $creneau['firstname']; ?></td>
+                    <td><?= $creneau['disponibilite']; ?></td>
+                    <td>
+                        <?php if ($creneau['disponibilite'] === 'dispo' && !isset($creneau['id_eleve'])) { ?>
+                            <a class="reserannu" href="reserver.php?id=<?= $creneau['id_creneaux']; ?>">Réserver</a>
+                        <?php } else { ?>
+                                <span>Réservé</span>
+                                <a href="annuler.php?id=<?= $creneau['id_creneaux']; ?>">Annuler</a>
+                            <?php } ?>
+                    </td>
+                </tr>
+            <?php } ?>
+
+
+
+
             </tbody>
         </table>
     </div>
